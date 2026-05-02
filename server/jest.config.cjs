be5c630a5f2b3b15f5f8542dc8e48706f5e64184
J33WAKASUPUN <supunprabodha789@gmail.com>
2026-05-03 02:58:35 +0530

    Add .gitignore and jest.config.cjs for project setup

---
+++ server/jest.config.cjs
@@ -0,0 +1,14 @@
+// jest.config.cjs
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
